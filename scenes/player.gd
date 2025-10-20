b50d62d61a698258514e8bc46ead2d665234b9a9
KuiMian <KuiMian42@gmail.com>
2025-10-20 03:25:50 +0200

    update input mapping

--- scenes/player.gd
+++ scenes/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 2.0
-const JUMP_VELOCITY = -400.0
-const ACCELERATION := 60
-const MAX_SPEED := 250
+const JUMP_VELOCITY := -400.0
+const ACCELERATION := 180
+const MAX_SPEED := 210
 const SHORT_JUMP_FACTOR := 8
+const FRICTION_FACTOR := 2 * ACCELERATION
 
 @onready var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 
 func _physics_process(delta: float) -> void:
@@ -24,12 +27,34 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x += direction * ACCELERATION * delta
-		velocity.x = clamp(velocity.x, - MAX_SPEED, MAX_SPEED)
-		#velocity.x = direction * SPEED
-	else:
-		#velocity.x = move_toward(velocity.x, 0, MAX_SPEED * 0.8 * delta)
-		velocity.x = lerp(.0, velocity.x, pow(0.2, delta))
+
+	#if direction:
+		#velocity.x += direction * ACCELERATION * delta
+		#velocity.x = clamp(velocity.x, - MAX_SPEED, MAX_SPEED)
+		##velocity.x = direction * SPEED
+	#
+	## 此时反向移动无摩擦力
+	#else:
+		#velocity.x = move_toward(velocity.x, 0, FRICTION_FACTOR * delta)
+		##velocity.x = lerp(.0, velocity.x, pow(0.2, delta))
+	
+	var target_speed = direction * MAX_SPEED if direction != 0 else 0.0
+	var acceleration = ACCELERATION if sign(direction) == sign(velocity.x) else FRICTION_FACTOR
+	velocity.x = move_toward(velocity.x, target_speed, acceleration * delta)
 
 	move_and_slide()
+	
+	update_animation()
+	check_facing_direction(direction)
+
+
+func update_animation() -> void:
+	if not is_on_floor():
+		animation_player.play("jump" if velocity.y > 0 else "fall")
+	else:
+		animation_player.play("move" if velocity.x != 0 else "idle")
+
+
+func check_facing_direction(direction: float) -> void:
+	if direction != 0:
+		sprite_2d.flip_h = direction < 0
